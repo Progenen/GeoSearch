8c0443abdd25c06ecead11dbdc0729d60b72cab7
Stanislav-Mac <stanislav_2010@icloud.com>
2024-11-02 02:33:24 +0500

    Start

--- dist/JS/bundle.js
+++ dist/JS/bundle.js
@@ -1,1 +1,32 @@
-document.addEventListener("DOMContentLoaded",(function(){}));
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/js/index.js":
+/*!*************************!*\
+  !*** ./src/js/index.js ***!
+  \*************************/
+/***/ (() => {
+
+eval("document.addEventListener('DOMContentLoaded', function () {\n    const burger = document.querySelector(\".header__burger-btn\");\n    const mobMenu = document.querySelector(\".mobmenu\");\n    const headerInfo = document.querySelector(\".header__info\");\n    const headerCallBtn = document.querySelector(\".header__call-btn\");\n    const headerMenu = document.querySelector(\".header__menu\");\n    const headerCall = document.querySelector(\".header__call\");\n    const header = document.querySelector(\"header\");\n\n    burger.addEventListener(\"click\", () => {\n        burger.classList.toggle(\"active\");\n        mobMenu.classList.toggle(\"active\");\n        document.body.classList.toggle(\"lock\");\n\n        mobMenu.style.paddingTop = header.getBoundingClientRect().height + \"px\";\n    });\n\n    if (window.innerWidth <= 992) {\n        mobMenu.querySelector(\".mobmenu__bottom\").append(headerInfo, headerCallBtn);\n        mobMenu.style.paddingTop = header.getBoundingClientRect().height + \"px\";\n    }\n\n    if (window.innerWidth <= 768) {\n        mobMenu.querySelector(\".mobmenu__top\").append(headerMenu);\n        mobMenu.querySelector(\".mobmenu__bottom\").append(headerCall);\n        mobMenu.querySelector(\".mobmenu__bottom\").append(headerCallBtn);\n        mobMenu.style.paddingTop = header.getBoundingClientRect().height + \"px\";\n    }\n});\n\n//# sourceURL=webpack://project-name/./src/js/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/js/index.js"]();
+/******/ 	
+/******/ })()
+;
--- src/JS/index.js
+++ src/JS/index.js
@@ -1,11 +1,29 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // const burger = document.querySelector(".header__burger");
-    // const menu = document.querySelector(".header__col--menu");
-    // const header = document.querySelector("header");
+    const burger = document.querySelector(".header__burger-btn");
+    const mobMenu = document.querySelector(".mobmenu");
+    const headerInfo = document.querySelector(".header__info");
+    const headerCallBtn = document.querySelector(".header__call-btn");
+    const headerMenu = document.querySelector(".header__menu");
+    const headerCall = document.querySelector(".header__call");
+    const header = document.querySelector("header");
 
-    // burger.addEventListener("click", () => {
-    //     burger.classList.toggle("active");
-    //     menu.classList.toggle("active");
-    //     document.body.classList.toggle("lock");
-    // });
+    burger.addEventListener("click", () => {
+        burger.classList.toggle("active");
+        mobMenu.classList.toggle("active");
+        document.body.classList.toggle("lock");
+
+        mobMenu.style.paddingTop = header.getBoundingClientRect().height + "px";
+    });
+
+    if (window.innerWidth <= 992) {
+        mobMenu.querySelector(".mobmenu__bottom").append(headerInfo, headerCallBtn);
+        mobMenu.style.paddingTop = header.getBoundingClientRect().height + "px";
+    }
+
+    if (window.innerWidth <= 768) {
+        mobMenu.querySelector(".mobmenu__top").append(headerMenu);
+        mobMenu.querySelector(".mobmenu__bottom").append(headerCall);
+        mobMenu.querySelector(".mobmenu__bottom").append(headerCallBtn);
+        mobMenu.style.paddingTop = header.getBoundingClientRect().height + "px";
+    }
 });
