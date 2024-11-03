ef4379b49a79916d96ad1b986f39fb71dba16aa3
Stanislav-Mac <stanislav_2010@icloud.com>
2024-11-04 04:03:54 +0500

    Pre end

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,18 @@
 const { src, dest, watch, series, parallel, tree } = require('gulp');
 const browserSync = require('browser-sync');
-const sass = require('gulp-sass')(require('sass'));                                
+const sass = require('gulp-sass')(require('sass'));
 const cleanCSS = require('gulp-clean-css');
 const autoprefixer = require('gulp-autoprefixer');
 const sprite = require('gulp-svg-sprite');
 const rename = require("gulp-rename");
-const imagemin = require('gulp-imagemin');
 const webp = require('gulp-webp');
 const newer = require('gulp-newer');
 const del = require('del');
 const fileinclude = require('gulp-file-include');
 const sourcemaps = require('gulp-sourcemaps');
 const gulpIf = require('gulp-if');
-const uglify = require('gulp-uglify');
-const concat = require('gulp-concat');
 const webpack = require('webpack-stream');
+const cacheBust = require('gulp-cache-bust');
 
 const isDevelopment = process.env.NODE_ENV == 'development' ? true : false; // Check work mode | Смотрим какой режим разработки выбран
 const dir = 'dist'; // Output | Папка с конечными файлами
@@ -25,7 +23,12 @@ function browsersync() {
         server: {
             baseDir: dir,
             index: "index.html"
-        }
+        },
+
+        notify: false,
+        open: true,
+        cors: true,
+
     });
 }
 
@@ -43,27 +46,25 @@ function style() {
 }
 
 // Просто перенос html файлов | Just transferring html files
-function html() {
+function processHtml() {
     return src('src/**/*.html')
         .pipe(fileinclude({
             prefix: '@@',
             basepath: '@file'
         }))
+        .on('error', (err) => console.error(err)) 
         .pipe(dest(dir + '/'))
 }
 
-// // Сборка JS модулей с помощью webpack | Building JS modules using webpack
-// function scripts() {
-//     return src([
-//         'src/JS/libs/**/*.js',
-//         'src/JS/index.js'
-//     ])
-//         .pipe(gulpIf(isDevelopment, sourcemaps.init())) // Инициализация source-maps (Работает только в режиме разработки) | Source-maps initialization (Only works in development mode)
-//         .pipe(concat('bundle.js'))
-//         .pipe(uglify())
-//         .pipe(gulpIf(isDevelopment, sourcemaps.write())) // Запись source-maps (Работает только в режиме разработки) | Source-maps entry (Only works in development mode)
-//         .pipe(dest(dir + '/JS/'))
-// }
+// Versioning html files to reset cache | Версионирования html файлов для сброса кеша
+function cacheBustTask() {
+    return src(dir + '/*.html')
+        .pipe(cacheBust({
+            type: 'timestamp'
+        }))
+        .pipe(dest(dir + '/'))
+        .pipe(browserSync.stream())
+}
 
 function scripts() {
     return src('src/js/*/**.js')
@@ -77,7 +78,7 @@ function scripts() {
                 rules: [
                     {
                         test: /\.css$/,
-                        use: ['style-loader', 'css-loader'], 
+                        use: ['style-loader', 'css-loader'],
                     }
                 ],
             },
@@ -89,14 +90,12 @@ function scripts() {
 function startWatch() {
     watch(['src/sass/**/*.+(scss|sass)'], style);
     watch(['src/JS/**/*.js', '!src/**/*.min.js'], scripts);
-    watch(['src/**/*.html'], html);
-    watch(['src/**/*.html']).on('change', browserSync.reload);
     watch(['src/images/**/*'], images);
-    watch(['src/svg/stack/mono/*'], svgspriteMono);
-    watch(['src/svg/stack/multi/*'], svgspriteMulti);
+    watch(['src/**/*.html'], processHtml);
+    watch(['src/**/*.html']).on('change', browserSync.reload);
+    watch(['src/svg/stack/mono/*'], series(svgspriteMono, cacheBustTask));
+    watch(['src/svg/stack/multi/*'], series(svgspriteMulti, cacheBustTask));
     watch(['src/fonts/**/*'], fonts);
-    watch(['src/svg/stack/mono/*'], svgspriteMono);
-    watch(['src/svg/stack/multi/*'], svgspriteMulti);
 }
 
 
@@ -131,7 +130,7 @@ function svgspriteMono() {
                 transform: [{
                     "svgo": {
                         "plugins": [
-                            { removeAttrs: { attrs: ['class', 'data-name', 'fill', 'stroke', 'color'] }}
+                            { removeAttrs: { attrs: ['class', 'data-name', 'fill', 'stroke', 'color'] } }
                         ]
                     }
                 }]
@@ -146,6 +145,7 @@ function svgspriteMono() {
 }
 
 function svgspriteMulti() {
+    console.log('Creating Multi SVG sprite...');
     return src('src/svg/stack/multi/*')
         .pipe(sprite({
             shape: {
@@ -183,7 +183,7 @@ function fonts() {
 exports.browsersync = browsersync;
 exports.scripts = scripts;
 exports.style = style;
-exports.html = html;
+exports.processHtml = processHtml;
 exports.cleanImg = cleanImg;
 exports.cleanFonts = cleanFonts;
 exports.fonts = fonts;
@@ -191,4 +191,7 @@ exports.svgspriteMulti = svgspriteMulti;
 exports.svgspriteMono = svgspriteMono;
 exports.video = video;
 
-exports.default = parallel(style, html, scripts, fonts, cleanFonts, svgspriteMono, svgspriteMulti, images, video, cleanImg, browsersync, startWatch);
+exports.default = series(
+    parallel(style, processHtml, scripts, fonts, cleanFonts, svgspriteMono, svgspriteMulti, images, video, cleanImg, browsersync, startWatch),
+
+);
--- src/JS/index.js
+++ src/JS/index.js
@@ -1,3 +1,7 @@
+import Swiper from "swiper";
+import {Navigation, Pagination} from "swiper/modules";
+import SimpleLightbox from "simplelightbox";
+
 document.addEventListener('DOMContentLoaded', function () {
     const burger = document.querySelector(".header__burger-btn");
     const mobMenu = document.querySelector(".mobmenu");
@@ -6,24 +10,231 @@ document.addEventListener('DOMContentLoaded', function () {
     const headerMenu = document.querySelector(".header__menu");
     const headerCall = document.querySelector(".header__call");
     const header = document.querySelector("header");
+    const firstSection = document.querySelector(".offer");
+    const docsItems = document.querySelectorAll(".docs__item");
+    const docsSliderWrapper = document.querySelector(".docs__slider .swiper-wrapper");
 
     burger.addEventListener("click", () => {
         burger.classList.toggle("active");
         mobMenu.classList.toggle("active");
-        document.body.classList.toggle("lock");
-
-        mobMenu.style.paddingTop = header.getBoundingClientRect().height + "px";
+        header.classList.toggle("active");
+        document.querySelector("html").classList.toggle("lock");
     });
+    
+    firstSection.style.paddingTop = header.getBoundingClientRect().height + "px";
+    mobMenu.style.paddingTop = header.getBoundingClientRect().height + "px";
 
     if (window.innerWidth <= 992) {
         mobMenu.querySelector(".mobmenu__bottom").append(headerInfo, headerCallBtn);
+
+        firstSection.style.paddingTop = header.getBoundingClientRect().height + "px";
         mobMenu.style.paddingTop = header.getBoundingClientRect().height + "px";
+
+        docsItems.forEach(el => {
+            const slide = document.createElement("div");
+            slide.classList.add("swiper-slide");
+
+            slide.append(el);
+
+            docsSliderWrapper.append(slide);
+        })
     }
 
     if (window.innerWidth <= 768) {
         mobMenu.querySelector(".mobmenu__top").append(headerMenu);
         mobMenu.querySelector(".mobmenu__bottom").append(headerCall);
         mobMenu.querySelector(".mobmenu__bottom").append(headerCallBtn);
+        
+        firstSection.style.paddingTop = header.getBoundingClientRect().height + "px";
         mobMenu.style.paddingTop = header.getBoundingClientRect().height + "px";
     }
+
+    const dataTabTitles = document.querySelectorAll("[data-tab-title]");
+    const dataTabContents = document.querySelectorAll("[data-tab-content]")
+
+    dataTabTitles.forEach((el, i) => {
+        el.addEventListener("click", (e) => {
+            if (el.classList.contains("active") === true) {
+                dataTabContents[i].style.height = "0px";
+                el.classList.remove("active");
+                dataTabContents[i].classList.remove("active");
+            } else {
+                dataTabTitles.forEach((elj, j) => {
+                    dataTabContents[j].style.height = 0 + "px";
+                    elj.classList.remove("active");
+                    dataTabContents[j].classList.remove("active");
+                })
+                dataTabContents[i].style.height = dataTabContents[i].scrollHeight + "px";
+                el.classList.add("active");
+                dataTabContents[i].classList.add("active");
+            }
+        });
+    });
+
+    // Sliders
+    const employeesSlider = new Swiper('.employees__slider', {
+        modules: [Navigation, Pagination],
+        slidesPerView: 1,
+        slidesPerGroup: 1,
+        spaceBetween: 5,
+        navigation: {
+            prevEl: '.employees__slider-arrow--prev',
+            nextEl: '.employees__slider-arrow--next',
+        },
+        pagination: {
+            el: '.employees__slider-pag',
+            clickable: true,
+            dynamicBullets: true
+        },
+        breakpoints: {
+            992: {
+                slidesPerView: 4,
+                slidesPerGroup: 2,
+                pagination: {
+                    dynamicBullets: false
+                }
+            },
+
+            768: {
+                slidesPerView: 2,
+                slidesPerGroup: 2,
+                pagination: {
+                    dynamicBullets: false
+                }
+            },
+
+            576: {
+                pagination: {
+                    dynamicBullets: false
+                }
+            }
+        }
+    })
+
+    const certificatesSlider = new Swiper('.certificates__slider', {
+        modules: [Navigation, Pagination],
+        slidesPerView: 1,
+        slidesPerGroup: 1,
+        spaceBetween: 5,
+        navigation: {
+            prevEl: '.certificates__slider-arrow--prev',
+            nextEl: '.certificates__slider-arrow--next',
+        },
+        pagination: {
+            el: '.certificates__slider-pag',
+            clickable: true,
+            dynamicBullets: true
+        },
+        breakpoints: {
+            992: {
+                slidesPerView: 4,
+                slidesPerGroup: 2,
+                pagination: {
+                    dynamicBullets: false
+                }
+            },
+
+            768: {
+                slidesPerView: 2,
+                slidesPerGroup: 2,
+                spaceBetween: 30,
+                pagination: {
+                    dynamicBullets: false
+                }
+            },
+
+            576: {
+                pagination: {
+                    dynamicBullets: false
+                }
+            }
+        }
+    })
+
+    const workpointSlider = new Swiper('.workpoint__slider', {
+        modules: [Navigation, Pagination],
+        slidesPerView: 1,
+        slidesPerGroup: 1,
+        spaceBetween: 5,
+        resistanceRatio: 1,
+        centeredSlides: true,
+        navigation: {
+            prevEl: '.workpoint__slider-arrow--prev',
+            nextEl: '.workpoint__slider-arrow--next',
+        },
+        pagination: {
+            el: '.workpoint__slider-pag',
+            clickable: true,
+            dynamicBullets: true
+        },
+        breakpoints: {
+            992: {
+                slidesPerView: 2.1,
+                slidesPerGroup: 1,
+                pagination: {
+                    dynamicBullets: false
+                }
+            },
+
+            768: {
+                slidesPerView: 1,
+                slidesPerGroup: 1,
+                centeredSlides: false,
+                spaceBetween: 0,
+                pagination: {
+                    dynamicBullets: false
+                }
+            },
+
+            576: {
+                pagination: {
+                    dynamicBullets: false
+                }
+            }
+        }
+    })
+
+    const docsSlider = new Swiper('.docs__slider', {
+        modules: [Navigation, Pagination],
+        slidesPerView: 1,
+        slidesPerGroup: 1,
+        spaceBetween: 5,
+        navigation: {
+            prevEl: '.docs__slider-arrow--prev',
+            nextEl: '.docs__slider-arrow--next',
+        },
+        pagination: {
+            el: '.docs__slider-pag',
+            clickable: true,
+            dynamicBullets: true
+        },
+        breakpoints: {
+            992: {
+                pagination: {
+                    dynamicBullets: false
+                }
+            },
+
+            768: {
+                pagination: {
+                    dynamicBullets: false
+                }
+            },
+
+            576: {
+                pagination: {
+                    dynamicBullets: false
+                }
+            }
+        }
+    })
+
+    // Gallery
+    const gallerys = document.querySelectorAll("[data-gallery]");
+
+    gallerys.forEach(el => {
+        new SimpleLightbox(el.querySelectorAll("a"))
+    })
+
+
 });
